0d8dbdaebf8fbfb68e8ec2985a97cbaadeb09dde
caravella483 <54142973+caravella483@users.noreply.github.com>
2020-02-13 21:04:08 -0500

    final_changes

--- resultsquery.sql
+++ resultsquery.sql
@@ -3,16 +3,17 @@ h.country,
 h.happiness_score,
 sum(s.suicides_no) AS suicides,
 sum(s.population) AS population,
-sum(s.suicides_no)/sum(s.population) AS Suicide_rate,
+cast(sum(s.suicides_no)*100.0/sum(s.population) as numeric(10,2)) AS Suicide_rate,
 CASE
-WHEN h.happiness_score< sum(s.suicides_no)/sum(s.population) Then 'unhappy_people'
-ELSE 'happy_people'
+WHEN h.happiness_score> h.health Then 'Happiness_score_is_fake_news'
+ELSE 'Happiness_score_is_real'
 END AS Are_they_happy
 from happy_country AS h
 left join suicide2016 AS s on h.country=s.country
 where s.country=h.country
 group by 
 h.country,
-h.happiness_score
+h.happiness_score,
+h.health
 
 
