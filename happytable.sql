977ae4d55349a67a96ee6abacbbaa39fe5960fdc
caravella483 <54142973+caravella483@users.noreply.github.com>
2020-02-15 10:06:16 -0500

    Final changes

--- happytable.sql
+++
@@ -1,9 +0,0 @@
-create table happy_country (
-ID SERIAl,
-Country varchar(30) primary key,
-Happiness_Score int,
-Family  int,
-Health  int,
-Freedom    int,
-Trust       int,
-Generosity      int);
